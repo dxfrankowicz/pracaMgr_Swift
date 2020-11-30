9edcdce42cd2134ad68134835a7833aca611d031
Damian Frankowicz <damianox3@gmail.com>
2020-11-30 13:26:43 -0800

    Layout fixes

--- pracaMgr_Swift/ContentView.swift
+++ pracaMgr_Swift/ContentView.swift
@@ -25,77 +25,95 @@ struct ContentView: View {
         operation()
         let elapsedTime = CFAbsoluteTimeGetCurrent() - startTime
         print("Czas wykonania: \(elapsedTime*1000) ms")
-        return "Czas wykonania: \(elapsedTime*1000) ms"
+        return String(format: "Czas wykonania:  %.2f ms", elapsedTime*1000)
     }
     
     var body: some View {
-        VStack(alignment: .leading, spacing: 0){
-            Button(action: {self.showFetchData.toggle();
-            })  {Text("Pobierz dane z API")}
-                .frame(minWidth: 0, maxWidth: .infinity)
-                .padding()
-                .background(Color.gray.opacity(0.1))
-                .cornerRadius(10)
-                .padding(.horizontal, 10)
-                .padding(.bottom, 10)
-            
-            Button(action: {self.showCreateWriteReadFile.toggle();
-            })  {Text("Stwórz i wyświetl dane z pliku")}
-                .frame(minWidth: 0, maxWidth: .infinity)
-                .padding()
-                .background(Color.gray.opacity(0.1))
-                .cornerRadius(10)
-                .padding(.horizontal, 10)
-                .padding(.bottom, 10)
-            
-            Button(action: {self.showDatabase.toggle();
-            })  {Text("Stwórz i pobierz dane z lokalnej bazy danych")}
-                .frame(minWidth: 0, maxWidth: .infinity)
-                .padding()
-                .background(Color.gray.opacity(0.1))
-                .cornerRadius(10)
-                .padding(.horizontal, 10)
-                .padding(.bottom, 10)
-            
-            Text(time)
-            
-            if showFetchData{
-                List(results)   { x in
-                    Text("UserId: \(x.userId), Id: \(x.id), Title: \(x.title), Completed: \(x.completed.description)")
+        VStack(alignment: .center, spacing: 0){
+            VStack(){
+                Button(action: {
+                    self.showFetchData = true
+                    self.showDatabase = false
+                    self.showCreateWriteReadFile = false
+                })  {Text("Pobierz dane z API")
+                    .multilineTextAlignment(.center)}
+                    .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
+                    .padding()
+                    .background(Color.gray.opacity(0.1))
+                    .cornerRadius(10)
+                    .padding(.horizontal, 10)
+                    .padding(.bottom, 10)
+                
+                Button(action: {
+                    self.showFetchData = false
+                    self.showDatabase = false
+                    self.showCreateWriteReadFile = true
+                })  {Text("Stwórz i wyświetl dane z pliku")
+                    .multilineTextAlignment(.center)}
+                    .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
+                    .padding()
+                    .background(Color.gray.opacity(0.1))
+                    .cornerRadius(10)
+                    .padding(.horizontal, 10)
+                    .padding(.bottom, 10)
+                
+                Button(action: {
+                    self.showFetchData = false
+                    self.showDatabase = true
+                    self.showCreateWriteReadFile = false
+                })  {Text("Stwórz i pobierz dane z lokalnej bazy danych")
+                    .multilineTextAlignment(.center)}
+                    .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
+                    .padding()
+                    .background(Color.gray.opacity(0.1))
+                    .cornerRadius(10)
+                    .padding(.horizontal, 10)
+                Text(time == "" ? "Czas operacji" : time)
+                    .multilineTextAlignment(.center)
+                    .frame(minWidth: 0, maxWidth: .infinity, alignment: .center)
+                    .padding()
+            }
+            .frame(height: UIScreen.main.bounds.size.height/2)
+            VStack(){
+                if showFetchData{
+                    List(results)   { x in
+                        Text("UserId: \(x.userId), Id: \(x.id), Title: \(x.title), Completed: \(x.completed.description)")
+                    }
+                    .onAppear(){
+                        self.time = self.getElapsedTimeInSeconds(operation: {ApiClient().fetchData { (results) in
+                            self.results = results
+                            }})
+                    }
                 }
-                .onAppear(){
-                    self.time = self.getElapsedTimeInSeconds(operation: {ApiClient().fetchData { (results) in
-                        self.results = results
-                        }})
+                
+                if showCreateWriteReadFile {
+                    Text(fileContent).onAppear(){
+                        self.time = self.getElapsedTimeInSeconds(operation: {FileUtils().createAndWriteFile{
+                            (data) in self.fileContent = data
+                            }})
+                    }
+                }
+                
+                if showDatabase{
+                    List(resultsDb)   { x in
+                        Text("Id: \(x.id), \(x.name) \(x.surname), Age: \(x.age)")
+                    }
+                    .onAppear(){
+                        self.time = self.getElapsedTimeInSeconds(operation: {
+                            let db:DBHelper = DBHelper()
+                            self.resultsDb = db.read()
+                        })
+                    }
                 }
             }
-            
-            if showCreateWriteReadFile {
-                Text(fileContent).onAppear(){
-                    self.time = self.getElapsedTimeInSeconds(operation: {FileUtils().createAndWriteFile{
-                        (data) in self.fileContent = data
-                        }})
-                }
-            }
-            
-            if showDatabase{
-                List(resultsDb)   { x in
-                    Text("Id: \(x.id), \(x.name) \(x.surname), Age: \(x.age)")
-                }
-                .onAppear(){
-                    self.time = self.getElapsedTimeInSeconds(operation: {
-                        let db:DBHelper = DBHelper()
-                        self.resultsDb = db.read()
-                    })
-                }
-            }
-            
+            .frame(height: UIScreen.main.bounds.size.height/2)
         }
+        .padding(.vertical, 10.0)
     }    
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView().frame(alignment: .top)
+        ContentView().padding().frame(alignment: .top)
     }
 }
